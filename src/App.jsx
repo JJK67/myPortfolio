41bb9415e7336a4e9afee46c9a355d02d426cb65
Jason <22098186@student.hhs.nl>
2024-04-09 18:34:19 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import {Nav} from '@components';
+
+function App() {
+  return (
+    <>
+      <Nav/>
+      <h1>Welkom!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+      <div className="nav">
+        <div>Home</div>
+        <div>Projects</div>
+        <div>About</div>
+      </div>
+    );
+  }
+  
+  export default Nav;
